70ace6fe7a0c5bef73b9915a292221fe97be44ad
123Benaiah <benaiahlushomo@gmail.com>
2026-09-23 12:20:37 +0200

    Register VerbalAutopsy in admin

--- va/admin.py
+++ va/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import VerbalAutopsy
+
+
+@admin.register(VerbalAutopsy)
+class VerbalAutopsyAdmin(admin.ModelAdmin):
+    # Columns shown in the admin list view
+    list_display = (
+        'deceased_name',
+        'sex',
+        'age_at_death',
+        'place_of_death',
+        'interview_date',
+    )
